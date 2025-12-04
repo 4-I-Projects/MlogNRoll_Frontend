e74d07746897c813ac63e0a76ac5b9a1b29e1a55
AnhVu264 <nguyenanhvu2642004@gmail.com>
2025-12-04 10:43:49 +0700

    made emotional themes

---
+++ src/themes/happyTheme.ts
@@ -0,0 +1,7 @@
+const happyTheme = {
+  background: "#FFF7D1",
+  text: "#333",
+  accent: "#FFD700"
+};
+
+export default happyTheme;
---
+++ src/themes/index.ts
@@ -0,0 +1,13 @@
+import happy from "./happyTheme";
+import sad from "./sadTheme";
+import angry from "./angryTheme";
+import tired from "./tiredTheme";
+import romantic from "./romanticTheme";
+
+export const themes = {
+  happy,
+  sad,
+  angry,
+  tired,
+  romantic
+};
